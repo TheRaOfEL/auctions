a472ca4dc3be838a5dc185c53f8e910e9d9fa9ac
TheRaOfEl <israelajayi149@gmail.com>
2025-07-28 09:43:56 +0100

    Added Railway domain to ALLOWED_HOSTS

--- commerce/settings.py
+++ commerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-530$by62yh(^(d08w!94kmog^7natqvhz52!tjcsja(l+d6k(2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['siegs-domain.onrender.com','127.0.0.1']
+ALLOWED_HOSTS = ['siegs-domain.onrender.com','127.0.0.1', 'sieg-auction.up.railway.app']
 
 # Application definition
 
